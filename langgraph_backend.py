358dad9c9a089a00d0bb299d7434fc29a430fa7e
Roy7721 <s2010724129@ru.ac.bd>
2026-08-23 20:05:24 +0600

    sqlite_database_added

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,11 +1,13 @@
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
 from typing import Annotated, TypedDict
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from dotenv import load_dotenv
 from langgraph.graph import START,StateGraph,END
 import os
+import sqlite3
+
 
 load_dotenv()
 
@@ -25,7 +27,8 @@ def chat_node(state: ChatState):
     return {"messages": [response]}
 
 # Checkpointer
-checkpointer = InMemorySaver()
+conn = sqlite3.connect(database= 'chatbot.db', check_same_thread = False)
+checkpointer = SqliteSaver(conn = conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -33,3 +36,25 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrive_all_threads():
+
+    all_thread = set()
+
+    for i in checkpointer.list(None):
+        all_thread.add(i.config["configurable"]['thread_id'])
+
+    return list(all_thread)
+
+
+
+# # test
+# user_input = "what is my name"
+# CONFIG = {"configurable" : {'thread_id' : '1'}}
+
+# response = chatbot.invoke(
+#                       {"messages": [HumanMessage(content = user_input)]}, 
+#                       config=CONFIG
+#                  )
+
+# print(response)
